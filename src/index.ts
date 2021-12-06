5ed7ec061e9671017e75bb65a202b8c97d466ed6
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-12-06 11:10:13 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+/* 
+Dados estos 5 arreglos que representan las ventas semanales del primer 
+trimentre, corresspondientes a 5 vendedores de la empresa: 
+Camila, Franco, Sofia, Matias y Agustina, respectivamente.
+vendedor1 [32558, 36165, 34240, 39273, 34360, 21347, 20869, 21493, 23062, 31908, 30369, 30652] 
+vendedor2 [27520, 31480, 24053, 34823, 32942, 38477, 23756, 21413, 26699, 22045, 25625, 26855] 
+vendedor3 [20584, 33473, 34300, 24598, 33955, 24040, 39173, 25542, 25105, 26759, 29790, 36218] 
+vendedor4 [27243, 38774, 21246, 30691, 24542, 39771, 31807, 31641, 20850, 29837, 37182, 28006] 
+vendedor5 [23334, 32687, 25217, 26844, 27033, 35244, 25702, 25781, 35525, 34874, 38842, 20562]
+
+Escribir un programa que permita determinar:Para cada vendedor mostrar su nombre y 
+1- su venta maxima indicando semana y mes de la misma.
+2- su venta minima indicando semana y mes de la misma.
+3- su promedio semanal de ventas.
+4- su promedio mensual de ventas.Entre todos los vendedores calcular:
+5- nombre y monto vendido del mejor vendedor de cada semana.
+6- nombre y suma de montos vendidos del mejor vendedor de cada mes
+7- nombre y suma de montos vendidos del peor vendedor de cada mes
+8- nombre y suma de montos vendidos de peor vendedor del trimestreTenga en cuenta que cada valor en los arreglos es la venta de una semana, y que cuatro semanas forman un mes
+*/
+
+let vendedor1: number[] = new Array(
+  32558,
+  36165,
+  34240,
+  39273,
+  34360,
+  21347,
+  20869,
+  21493,
+  23062,
+  31908,
+  30369,
+  30652
+);
