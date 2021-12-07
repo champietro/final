b3592f5312a01e47131c44cb70ee092de8d70f99
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-12-07 19:13:35 -0300

    dfgfdg

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 /* 
-Dados estos 5 arreglos que representan las ventas semanales del primer 
-trimentre, corresspondientes a 5 vendedores de la empresa: 
-Camila, Franco, Sofia, Matias y Agustina, respectivamente.
-vendedor1 [32558, 36165, 34240, 39273, 34360, 21347, 20869, 21493, 23062, 31908, 30369, 30652] 
-vendedor2 [27520, 31480, 24053, 34823, 32942, 38477, 23756, 21413, 26699, 22045, 25625, 26855] 
-vendedor3 [20584, 33473, 34300, 24598, 33955, 24040, 39173, 25542, 25105, 26759, 29790, 36218] 
-vendedor4 [27243, 38774, 21246, 30691, 24542, 39771, 31807, 31641, 20850, 29837, 37182, 28006] 
-vendedor5 [23334, 32687, 25217, 26844, 27033, 35244, 25702, 25781, 35525, 34874, 38842, 20562]
-
-Escribir un programa que permita determinar:Para cada vendedor mostrar su nombre y 
-1- su venta maxima indicando semana y mes de la misma.
-2- su venta minima indicando semana y mes de la misma.
-3- su promedio semanal de ventas.
-4- su promedio mensual de ventas.Entre todos los vendedores calcular:
-5- nombre y monto vendido del mejor vendedor de cada semana.
-6- nombre y suma de montos vendidos del mejor vendedor de cada mes
-7- nombre y suma de montos vendidos del peor vendedor de cada mes
-8- nombre y suma de montos vendidos de peor vendedor del trimestreTenga en cuenta que cada valor en los arreglos es la venta de una semana, y que cuatro semanas forman un mes
+	Dados estos 5 arreglos que representan las ventas semanales del primer 
+	trimentre, corresspondientes a 5 vendedores de la empresa: 
+	Camila, Franco, Sofia, Matias y Agustina, respectivamente.
+	vendedor1 [32558, 36165, 34240, 39273, 34360, 21347, 20869, 21493, 23062, 31908, 30369, 30652] 
+	vendedor2 [27520, 31480, 24053, 34823, 32942, 38477, 23756, 21413, 26699, 22045, 25625, 26855] 
+	vendedor3 [20584, 33473, 34300, 24598, 33955, 24040, 39173, 25542, 25105, 26759, 29790, 36218] 
+	vendedor4 [27243, 38774, 21246, 30691, 24542, 39771, 31807, 31641, 20850, 29837, 37182, 28006] 
+	vendedor5 [23334, 32687, 25217, 26844, 27033, 35244, 25702, 25781, 35525, 34874, 38842, 20562]
+	
+	Escribir un programa que permita determinar:Para cada vendedor mostrar su nombre y 
+	1- su venta maxima indicando semana y mes de la misma.
+	2- su venta minima indicando semana y mes de la misma.
+	3- su promedio semanal de ventas.
+	4- su promedio mensual de ventas.Entre todos los vendedores calcular:
+	5- nombre y monto vendido del mejor vendedor de cada semana.
+	6- nombre y suma de montos vendidos del mejor vendedor de cada mes
+	7- nombre y suma de montos vendidos del peor vendedor de cada mes
+	8- nombre y suma de montos vendidos de peor vendedor del trimestreTenga en cuenta que cada valor en los arreglos es la venta de una semana, y que cuatro semanas forman un mes
 */
 
 let vendedor1: number[] = new Array(
@@ -33,7 +33,6 @@ let vendedor1: number[] = new Array(
   30369,
   30652
 );
-
 let vendedor2: number[] = new Array(
   27520,
   31480,
@@ -48,7 +47,6 @@ let vendedor2: number[] = new Array(
   25625,
   26855
 );
-
 let vendedor3: number[] = new Array(
   20584,
   33473,
@@ -63,7 +61,6 @@ let vendedor3: number[] = new Array(
   29790,
   36218
 );
-
 let vendedor4: number[] = new Array(
   27243,
   38774,
@@ -78,7 +75,6 @@ let vendedor4: number[] = new Array(
   37182,
   28006
 );
-
 let vendedor5: number[] = new Array(
   23334,
   32687,
@@ -133,7 +129,74 @@ function mejorVendedorMes(
   ventas4,
   ventas5: number[],
   nombres: string[]
-) {}
+) {
+  let v1,
+    v2,
+    v3,
+    v4,
+    v5,
+    maxima: number = 0;
+  let mes: number;
+  let vendedor: string;
+  for (let i = 0; i < 12; i++) {
+    v1 = v1 + ventas1[i];
+    v2 = v2 + ventas2[i];
+    v3 = v3 + ventas3[i];
+    v4 = v4 + ventas4[i];
+    v5 = v5 + ventas5[i];
+    switch (i) {
+      case 3: {
+        mes = 1;
+        maxima = Math.max(v1, v2, v3, v4, v5);
+        if (maxima == v1) {
+          vendedor = nombres[0];
+        } else {
+          if (maxima == v2) {
+            vendedor = nombres[1];
+          } else {
+            if (maxima == v3) {
+              vendedor = nombres[2];
+            } else {
+              if (maxima == v4) {
+                vendedor = nombres[3];
+              } else {
+                vendedor = nombres[4];
+              }
+            }
+          }
+        }
+        console.log(vendedor + " fué quien más vendió en el mes ");
+        v1 = 0;
+        v2 = 0;
+        v3 = 0;
+        v4 = 0;
+        v5 = 0;
+        break;
+      }
+      case 7: {
+        mes = 2;
+        console.log(vendedor + " fué quien más vendió en el mes ");
+        v1 = 0;
+        v2 = 0;
+        v3 = 0;
+        v4 = 0;
+        v5 = 0;
+        break;
+      }
+      case 11: {
+        mes = 3;
+        console.log(vendedor + " fué quien más vendió en el mes ");
+        v1 = 0;
+        v2 = 0;
+        v3 = 0;
+        v4 = 0;
+        v5 = 0;
+        break;
+      }
+    }
+  }
+}
+
 function mejorVendedorSemana(
   ventas1,
   ventas2,
@@ -291,7 +354,6 @@ mejorVendedorSemana(
   vendedor5,
   nombres
 );
-
 mejorVendedorMes(
   vendedor1,
   vendedor2,
